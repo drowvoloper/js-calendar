03ae8cbda45cfb1834d01f6930540a02e0ac8957
Logan <logan@bglp.es>
2020-03-03 21:05:29 +0100

    functions improved

--- app.js
+++ app.js
@@ -4,38 +4,48 @@ const currentYear = today.getFullYear();
 const currentMonth = today.getMonth();
 const currentDate = today.getDate();
 
+// declarate elements
+const monthDropdown = document.getElementById("chooseMonth");
+const months = document.getElementsByClassName("month");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+// Set default values
 let chosenMonth = currentMonth;
 let chosenYear = currentYear;
 
-const moveList = (element, value) => {
-  element.style.transform = "translateY(" + (-80 * value) + "px)";
+// --> USEFUL FUNCTIONS YAYYYYYYYYYYY!! <--
+// move lists when the value is changed
+const moveList = (element, value) => element.style.transform = "translateY(" + (-80 * value) + "px)";
+
+// show / hide months list
+const hideMonths = () => {
+  monthDropdown.classList.remove("open");
+  for (let j = 0; j < months.length; j++)
+    if (j != chosenMonth) months[j].classList.remove("current-month");
+};
+const showMonths = () => {
+  monthDropdown.classList.add("open");
+  for (let j = 0; j < months.length; j++)
+    if (j != chosenMonth) months[j].classList.add("current-month");
 };
 
 // Style lists to show the default(or current) value
 // months list
-const monthDropdown = document.getElementById("chooseMonth");
 moveList(monthDropdown, chosenMonth);
-const months = document.getElementsByClassName("month");
 months[currentMonth].classList.add("current-month");
 
-const showMonths = index => months[index].style.opacity = "1";
-
+// addEventListener to each element of the months list
 for (let i = 0; i < months.length; i++)
 {
   months[i].addEventListener("click", () => {
     // show the list when current/showed month is clicked
-    if (months[i].value == chosenMonth && !monthDropdown.classList.contains("open"))
-    {
-      monthDropdown.classList.add("open");
-      for (let j = 0; j < months.length; j++)
-        months[j].classList.add("current-month");
-    }
+    if (months[i].value == chosenMonth)
+      showMonths();
     // and move the list when a different month is clicked
     else {
-      monthDropdown.classList.remove("open");
-      for (let j = 0; j < months.length; j++)
-        months[j].classList.remove("current-month");
-      //months[chosenMonth].classList.remove("current-month");
+      hideMonths();
+      months[chosenMonth].classList.remove("current-month");
       chosenMonth = months[i].value;
       months[chosenMonth].classList.add("current-month");
       moveList(monthDropdown, chosenMonth);
@@ -45,13 +55,8 @@ for (let i = 0; i < months.length; i++)
 
 // EventListener for next and prev buttons
 // month changes when they are clicked
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
-
 prevBtn.addEventListener("click", () => {
-  monthDropdown.classList.remove("open");
-  for (let j = 0; j < months.length; j++)
-    months[j].classList.remove("current-month");
+  if (monthDropdown.classList.contains("open")) hideMonths();
   if(chosenMonth > 0)
   {
     months[chosenMonth].classList.remove("current-month");
@@ -60,9 +65,7 @@ prevBtn.addEventListener("click", () => {
   }
 });
 nextBtn.addEventListener("click", () => {
-  monthDropdown.classList.remove("open");
-  for (let j = 0; j < months.length; j++)
-    months[j].classList.remove("current-month");
+  if (monthDropdown.classList.contains("open")) hideMonths();
   if(chosenMonth < 11)
   {
     months[chosenMonth].classList.remove("current-month");
