d8ef108186325bff0b70641ff3c971821f35ebc8
Logan <logan@bglp.es>
2020-03-03 20:12:00 +0100

    month list opens and closes

--- app.js
+++ app.js
@@ -16,12 +16,30 @@ const moveList = (element, value) => {
 const monthDropdown = document.getElementById("chooseMonth");
 moveList(monthDropdown, chosenMonth);
 const months = document.getElementsByClassName("month");
+months[currentMonth].classList.add("current-month");
+
+const showMonths = index => months[index].style.opacity = "1";
 
 for (let i = 0; i < months.length; i++)
 {
   months[i].addEventListener("click", () => {
-    chosenMonth = months[i].value;
-    moveList(monthDropdown, chosenMonth);
+    // show the list when current/showed month is clicked
+    if (months[i].value == chosenMonth && !monthDropdown.classList.contains("open"))
+    {
+      monthDropdown.classList.add("open");
+      for (let j = 0; j < months.length; j++)
+        months[j].classList.add("current-month");
+    }
+    // and move the list when a different month is clicked
+    else {
+      monthDropdown.classList.remove("open");
+      for (let j = 0; j < months.length; j++)
+        months[j].classList.remove("current-month");
+      //months[chosenMonth].classList.remove("current-month");
+      chosenMonth = months[i].value;
+      months[chosenMonth].classList.add("current-month");
+      moveList(monthDropdown, chosenMonth);
+    }
   });
 }
 
@@ -31,8 +49,24 @@ const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 
 prevBtn.addEventListener("click", () => {
-  if(chosenMonth > 0) moveList(monthDropdown, --chosenMonth)
+  monthDropdown.classList.remove("open");
+  for (let j = 0; j < months.length; j++)
+    months[j].classList.remove("current-month");
+  if(chosenMonth > 0)
+  {
+    months[chosenMonth].classList.remove("current-month");
+    moveList(monthDropdown, --chosenMonth);
+    months[chosenMonth].classList.add("current-month");
+  }
 });
 nextBtn.addEventListener("click", () => {
-  if(chosenMonth < 11) moveList(monthDropdown, ++chosenMonth)
+  monthDropdown.classList.remove("open");
+  for (let j = 0; j < months.length; j++)
+    months[j].classList.remove("current-month");
+  if(chosenMonth < 11)
+  {
+    months[chosenMonth].classList.remove("current-month");
+    moveList(monthDropdown, ++chosenMonth);
+    months[chosenMonth].classList.add("current-month");
+  }
 });
